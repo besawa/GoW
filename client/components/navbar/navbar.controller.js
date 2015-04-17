71a11b40fb1a85eccf9d8b0a8e87d819e936da6e
mkinawy <mohammed.kinawy@gmail.com>
2015-04-17 20:12:12 +0200

    showing page routes in menu

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,23 +1,32 @@
 'use strict';
 
 angular.module('gowApp')
-  .controller('NavbarCtrl', function ($scope, $location, Auth) {
+  .controller('NavbarCtrl', function ($scope, $location, Auth, Pages) {
     $scope.menu = [
       {'title': 'Home', 'link': '/'},
       {'title': 'Pages', 'link': '/pages'}
     ];
+
+    // adding pages routes only if menu enabled
+    var pages = Pages.query(function () {
+      _.forEach(pages, function (page) {
+        if (page.menu) {
+          $scope.menu.push({title: page.name, link: page.route});
+        }
+      });
+    });
 
     $scope.isCollapsed = true;
     $scope.isLoggedIn = Auth.isLoggedIn;
     $scope.isAdmin = Auth.isAdmin;
     $scope.getCurrentUser = Auth.getCurrentUser;
 
-    $scope.logout = function() {
+    $scope.logout = function () {
       Auth.logout();
       $location.path('/login');
     };
 
-    $scope.isActive = function(route) {
+    $scope.isActive = function (route) {
       return route === $location.path();
     };
   });
